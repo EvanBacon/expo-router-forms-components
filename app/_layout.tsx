1d36a4639d49cebd03d5e40ef44981f8630c083e
evanbacon <bacon@expo.io>
2025-04-24 21:52:39 -0500

    add async fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,35 @@
 import ThemeProvider from "@/components/ui/ThemeProvider";
 
 import Tabs from "@/components/ui/Tabs";
+import { SplashScreen } from "expo-router";
+import { Suspense, useEffect } from "react";
+
+// SplashScreen.preventAutoHideAsync();
+
+function SplashFallback() {
+  useEffect(
+    () => () => {
+      SplashScreen.hideAsync();
+    },
+    []
+  );
+  return null;
+}
 
 export default function Layout() {
   return (
-    <ThemeProvider>
-      <Tabs>
-        <Tabs.Screen name="(index)" systemImage="house.fill" title="Home" />
-        <Tabs.Screen
-          name="(info)"
-          systemImage="cursorarrow.rays"
-          title="Info"
-        />
-      </Tabs>
-    </ThemeProvider>
+    <Suspense fallback={<SplashFallback />}>
+      {/* <AsyncFont src={SourceCodePro_400Regular} fontFamily="Source Code Pro" /> */}
+      <ThemeProvider>
+        <Tabs>
+          <Tabs.Screen name="(index)" systemImage="house.fill" title="Home" />
+          <Tabs.Screen
+            name="(info)"
+            systemImage="cursorarrow.rays"
+            title="Info"
+          />
+        </Tabs>
+      </ThemeProvider>
+    </Suspense>
   );
 }
